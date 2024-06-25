4403e6f37f93a38e91a63ba08506970bf419ad47
Jeff Winget <jeff@3dgradebook.com>
2024-06-25 16:03:30 -0600

    Source wallust config

--- home.nix
+++ home.nix
@@ -38,11 +38,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # Source dotfiles to .config
     ".config/alacritty".source = ./configs/alacritty;
+    ".config/wallust".source = ./configs/wallust;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
