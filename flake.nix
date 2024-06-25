fb5bfc7c99b995502319a62028a610c5826e454e
Jeff Winget <jeff@3dgradebook.com>
2024-06-25 14:18:41 -0600

    configuring home-manager

--- flake.nix
+++ flake.nix
@@ -3,17 +3,26 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = { self, nixpkgs, ... }: 
+    outputs = { self, nixpkgs, home-manager, ... }: 
         let 
             lib = nixpkgs.lib;
             system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
         in {
         nixosConfigurations = {
             darter-pro = lib.nixosSystem {
                 inherit system;
                 modules = [ ./configuration.nix ];
+            };
+        };
+        homeConfigurations = {
+            wingej0 = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                modules = [ ./home.nix ];
             };
         };
     };
